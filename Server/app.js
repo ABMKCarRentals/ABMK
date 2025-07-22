6b98a4fdd11fd5cb021426518a1865bb189eb885
neekunjchaturvedi <neekunjchaturvedi3@gmail.com>
2025-07-22 11:31:06 +0530

    fix:cors

--- Server/app.js
+++ Server/app.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://abmk.vercel.app/"],
+    origin: ["http://localhost:5173", "https://abmk.vercel.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
