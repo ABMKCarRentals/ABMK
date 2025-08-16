97f4fe7ebef4b26dec2d359f91422a26cc6e9f41
neekunjchaturvedi <neekunjchaturvedi3@gmail.com>
2025-08-16 13:41:22 +0530

    fix: cors

--- Server/app.js
+++ Server/app.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://www.abmkcarrental.com/"],
+    origin: ["http://localhost:5173", "https://www.abmkcarrental.com"],
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
     allowedHeaders: [
       "Content-Type",
